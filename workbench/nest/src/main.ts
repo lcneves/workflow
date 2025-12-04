7b3eb4cda2950dd4e4bc2fcd5e1840162d0a538e
Adrian Lam <me@adriandlam.com>
2025-12-03 17:02:23 -0800

    add pg init for ci test

--- workbench/nest/src/main.ts
+++ workbench/nest/src/main.ts
@@ -4,6 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  // Start the Postgres World
+  // Needed since we test this in CI
+  if (process.env.NEXT_RUNTIME !== 'edge') {
+    // kickstart the world
+    import('workflow/runtime').then(async ({ getWorld }) => {
+      await getWorld().start?.();
+    });
+  }
 }
 
 bootstrap();
