cf99ec326b602f657db27fe5923b4ec8a0de087b
Hayden Bleasel <hello@haydenbleasel.com>
2025-12-05 12:25:29 -0800

    Manual fixes

--- docs/app/[lang]/layout.tsx
+++ docs/app/[lang]/layout.tsx
@@ -2,16 +2,26 @@ import '../global.css';
 import { Footer } from '@/components/geistdocs/footer';
 import { Navbar } from '@/components/geistdocs/navbar';
 import { GeistdocsProvider } from '@/components/geistdocs/provider';
+import { mono, sans } from '@/lib/geistdocs/fonts';
+import { cn } from '@/lib/utils';
 
 const Layout = async ({ children, params }: LayoutProps<'/[lang]'>) => {
   const { lang } = await params;
 
   return (
-    <GeistdocsProvider lang={lang}>
-      <Navbar />
-      {children}
-      <Footer />
-    </GeistdocsProvider>
+    <html
+      className={cn(sans.variable, mono.variable, 'scroll-smooth antialiased')}
+      lang={lang}
+      suppressHydrationWarning
+    >
+      <body>
+        <GeistdocsProvider lang={lang}>
+          <Navbar />
+          {children}
+          <Footer />
+        </GeistdocsProvider>
+      </body>
+    </html>
   );
 };
 
